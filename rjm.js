ba9f462ff4d5b4dc8d8981ca822265760ff279d8
arpruss <arpruss@gmail.com>
2020-11-20 11:26:57 -0600

    foo

--- rjm.js
+++ rjm.js
@@ -6,7 +6,7 @@ class RaspberryJamMod {
         this.block = "1";
         this.penDown = true;
         this.nib = [[0,0,0]];
-        this.ws = null;
+        this.socket = null;
     }
     
     getInfo() {
@@ -36,6 +36,29 @@ class RaspberryJamMod {
                         },
                     }
             },            
+            {
+                    "opcode": "setBlock",
+                    "blockType": "command",
+                    "text": "At ([x],[y],[z]) put block [b]",
+                    "arguments": {
+                        "x": {
+                            "type": "number",
+                            "defaultValue": "0"
+                        },
+                        "y": {
+                            "type": "number",
+                            "defaultValue": "0"
+                        },
+                        "z": {
+                            "type": "number",
+                            "defaultValue": "0"
+                        },
+                        "b": {
+                            "type": "number",
+                            "defaultValue": "1"
+                        },
+                    }
+            },            
             ],
         "menus": {}
         };
@@ -44,15 +67,133 @@ class RaspberryJamMod {
     connect({ip}){
         this.ip = ip;
         console.log("connecting to "+ip);
-        this.ws = new WebSocket("ws://"+ip+":14711");
+        this.socket = new WebSocket("ws://"+ip+":14711");
     };
     
     chat({msg}){
-        this.ws.send("chat.post("+msg+")");
+        this.socket.send("chat.post("+msg+")");
         console.log("chat: "+msg);
     };
     
+    mmMultiply(a,b) {
+        var c = [[0,0,0],[0,0,0],[0,0,0]];
+        for (var i = 0; i < 3 ; i++) for (var j = 0; j < 3 ; j++)
+          c[i][j] = a[i][0]*b[0][j] + a[i][1]*b[1][j] + a[i][2]*b[2][j];
+        return c;
+    };
     
+    yawMatrix(angleDegrees) {
+        var theta = angleDegrees * MCPI.TO_RADIANS;
+        return [[Math.cos(theta), 0., -Math.sin(theta)],
+                [0.,         1., 0.],
+                [Math.sin(theta), 0., Math.cos(theta)]];
+    };
+    
+    rollMatrix(angleDegrees) {
+        var theta = angleDegrees * MCPI.TO_RADIANS;
+        return [[Math.cos(theta), -Math.sin(theta), 0.],
+                [Math.sin(theta), Math.cos(theta),0.],
+                [0.,          0.,          1.]];
+    };
+    
+    pitchMatrix(angleDegrees) {
+        var theta = angleDegrees * MCPI.TO_RADIANS;
+        return [[1.,          0.,          0.],
+                [0., Math.cos(theta),Math.sin(theta)],
+                [0., -Math.sin(theta),Math.cos(theta)]];
+    };
+    
+    getLine(x1,y1,z1,x2,y2,z2) {
+        var line = [];
+        x1 = Math.floor(x1);
+        y1 = Math.floor(y1);
+        z1 = Math.floor(z1);
+        x2 = Math.floor(x2);
+        y2 = Math.floor(y2);
+        z2 = Math.floor(z2);
+        var point = [x1,y1,z1];
+        var dx = x2 - x1;
+        var dy = y2 - y1;
+        var dz = z2 - z1;
+        var x_inc = dx < 0 ? -1 : 1;
+        var l = Math.abs(dx);
+        var y_inc = dy < 0 ? -1 : 1;
+        var m = Math.abs(dy);
+        var z_inc = dz < 0 ? -1 : 1;
+        var n = Math.abs(dz);
+        var dx2 = l * 2;
+        var dy2 = m * 2;
+        var dz2 = n * 2;
+
+        if (l >= m && l >= n) {
+            var err_1 = dy2 - l;
+            var err_2 = dz2 - l;
+            for (var i=0; i<l; i++) {
+                line.push([point[0],point[1],point[2]]);
+                if (err_1 > 0) {
+                    point[1] += y_inc;
+                    err_1 -= dx2;
+                }
+                if (err_2 > 0) {
+                    point[2] += z_inc;
+                    err_2 -= dx2;
+                }
+                err_1 += dy2;
+                err_2 += dz2;
+                point[0] += x_inc;
+            }
+        }
+        else if (m >= l && m >= n) {
+            err_1 = dx2 - m;
+            err_2 = dz2 - m;
+            for (var i=0; i<m; i++) {
+                line.push([point[0],point[1],point[2]]);
+                if (err_1 > 0) {
+                    point[0] += x_inc;
+                    err_1 -= dy2;
+                }
+                if (err_2 > 0) {
+                    point[2] += z_inc;
+                    err_2 -= dy2;
+                }
+                err_1 += dx2;
+                err_2 += dz2;
+                point[1] += y_inc;
+            }
+        }
+        else {
+            err_1 = dy2 - n;
+            err_2 = dx2 - n;
+            for (var i=0; i < n; i++) {
+                line.push([point[0],point[1],point[2]]);
+                if (err_1 > 0) {
+                    point[1] += y_inc;
+                    err_1 -= dz2;
+                }
+                if (err_2 > 0) {
+                    point[0] += x_inc;
+                    err_2 -= dz2;
+                }
+                err_1 += dy2;
+                err_2 += dx2;
+                point[2] += z_inc;
+            }
+        }
+        line.push([point[0],point[1],point[2]]);
+        if (point[0] != x2 || point[1] != y2 || point[2] != z2) {
+            line.push([x2,y2,z2]);
+        }
+        return line;
+    };
+    
+    setBlock(x,y,z,block) {
+/*      if (block != "0" && Math.floor(x) == Math.floor(this.playerX) && Math.floor(z) == Math.floor(this.playerZ)
+          && (Math.floor(y) >= this.playerShiftedHeight) ) {
+            this.playerShiftedHeight = Math.floor(y) + 1;
+            this.socket.send("player.setPos("+this.playerX+","+this.playerShiftedHeight+","+this.playerZ+")");
+      } */
+      this.socket.send("world.setBlock("+x+","+y+","+z+","+block+")");
+    };
 }
 
 rjm = new RaspberryJamMod();
