16067755763f8df8b4ba13ca6747faf6faaadbb0
arpruss <arpruss@gmail.com>
2020-11-20 16:28:49 -0600

    foo

--- rjm.js
+++ rjm.js
@@ -114,7 +114,7 @@ class RaspberryJamMod {
             rjm.socket.onerror = function(err) {
                 reject(err);
             };
-        }).then(result -> { console.log("res "+result); })
+        }).then(result => { console.log("res "+result); })
             .then(rjm.getPosition())
           .then(result => { console.log("position "+result); })
           .then(rjm.getRotation())
