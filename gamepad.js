e49e20c3ae7868e7da557ba2c3909476c791e65f
arpruss <arpruss@gmail.com>
2020-12-21 09:45:42 -0600

    foo

--- gamepad.js
+++ gamepad.js
@@ -219,8 +219,8 @@ class ScratchGamepad {
             "menus": {
                 "pressReleaseMenu": [{text:"press",value:1}, {text:"release",value:0}],
                 "padMenu": {
-                            acceptReporters=true, 
-                            items=[{text:"1",value:1}, {text:"2",value:2}, {text:"3",value:3}, {text:"4",value:4}],
+                            acceptReporters: true, 
+                            items: [{text:"1",value:1}, {text:"2",value:2}, {text:"3",value:3}, {text:"4",value:4}],
                 }
             }            
         };
